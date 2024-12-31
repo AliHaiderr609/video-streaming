1b50941137d05eba90843a8bf42815a37bb8ca57
Ali Haider <alih659934@gmail.com>
2024-12-31 08:44:06 -0800

    updated baseUrl and built static files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require("path");
 const authRoutes = require('./routes/authRoutes');
 const videoRoutes = require('./routes/videoRoutes');
 const commentRoutes = require('./routes/commentRoutes');
@@ -23,6 +24,12 @@ mongoose.connect('mongodb+srv://alih65993:jIu8RujuwFfuxX2V@cluster0.xg0il.mongod
 app.use('/api/auth', authRoutes);
 app.use('/api/videos', videoRoutes);
 app.use('/api/comments', commentRoutes);
+
+//Production
+app.use(express.static("./tiktok-clone/build"));
+app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "tiktok-clone", "build", "index.html"))
+})
 
 // Start the server
 app.listen(PORT, () => {
--- tiktok-clone/src/services/api.js
+++ tiktok-clone/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://mern-stream-hjefg9e9cfa5fjhz.eastus-01.azurewebsites.net" });
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token");
